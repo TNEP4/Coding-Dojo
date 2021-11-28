2968e675d1db6025364de7377f1c9677991d05dc
Nic Touron <nicolas.t.touron@gmail.com>
2021-11-28 12:05:28 +1100

    MERN - Week 1 - All assignments

---
+++ MERN/week-1/first-jsx/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <div>
+        <li>Learn React</li>  
+        <li>Climb Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+      </div>
+    </div>
+  );
+}
+
+export default App;
