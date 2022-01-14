7e9d02b66312162018d2b8677066fb4a92de7f4a
Nic Touron <nicolas.touron@kedgebs.com>
2022-01-14 16:46:55 +0100

    Setting DB and config

---
+++ MERN/week-5/full-stack-app/server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+    origin: 'http://localhost:3000',
+}));
+
+// need to require config file
+require('./config/mongoose.config');
+// need to require routes
+
+app.listen(8000, () => {
+    console.log('Server is listening on port 8000');
+});
